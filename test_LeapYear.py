e7e586b2f8c5099be849a0323426f7e61a75adc6
Alanna <croysdaa@oregonstate.edu>
2021-06-07 15:23:04 -0700

    Created test for divisibility by 400

--- test_LeapYear.py
+++ test_LeapYear.py
@@ -8,6 +8,9 @@ class LeapYearTest(unittest.TestCase):
 
     def test_notDivByHundred(self):
         self.assertFalse(LeapYear.leapyear_check(100))
+    
+    def test_divByFourHundred(self):
+        self.assertTrue(LeapYear.leapyear_check(400))
 
 if __name__ == '__main__':
     unittest.main()
