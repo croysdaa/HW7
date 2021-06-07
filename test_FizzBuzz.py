c2342e1646862285fd9082f441ec229ddac3779f
Alanna <croysdaa@oregonstate.edu>
2021-06-07 14:46:26 -0700

    Created test for Fizz / 3

--- test_FizzBuzz.py
+++ test_FizzBuzz.py
@@ -7,6 +7,9 @@ class FizzBuzzTest(unittest.TestCase):
     def test_one(self):
        self.assertEqual(1, FizzBuzz.fizzbuzz_check(1))
 
+    def test_threes(self):
+        self.assertEqual("Fizz", FizzBuzz.fizzbuzz_check(9))
+
 if __name__ == '__main__':
     unittest.main()
 
