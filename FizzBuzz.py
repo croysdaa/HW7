68d74b6ce51936ea7fc4f34a1f5469f3fb5a14bd
Alanna <croysdaa@oregonstate.edu>
2021-06-07 15:02:29 -0700

    Created code for printing

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,9 +1,14 @@
 def fizzbuzz_check(a):
+    
     if (a % 3 == 0):
         if (a % 5 == 0):
             return "FizzBuzz"
-        return "Fizz" 
+        return "Fizz"
+
     if (a % 5 == 0):
-        return "Buzz"
+        return "Buzz" 
     
     return a
+
+for i in range(1, 101):
+    print(fizzbuzz_check(i))
